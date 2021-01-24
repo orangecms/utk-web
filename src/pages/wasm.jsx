24a73b99ed9f2d45b6efe5765acb7fe06b4214af
Daniel Maslowski <info@orangecms.org>
2021-01-24 17:51:45 +0100

    WIP

--- src/pages/wasm.jsx
+++ src/pages/wasm.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
-// import gogo from "../go/main.go";
-import dynamic from "next/dynamic";
-const gogo = dynamic(() => import("../go/main.go"));
+import gogo from "../go/main.go";
+//import dynamic from "next/dynamic";
+//const gogo = dynamic(() => import("../go/main.go"));
 
 const Page = () => {
   const [v, setV] = useState(0);
+  console.info({ gogo });
   useEffect(() => {
-    const { add } = gogo();
-    add(1, 2).then((x) => setV(x));
+    try {
+      const { add } = gogo;
+      add(1, 2).then((x) => setV(x));
+    } catch (e) {
+      console.error(e);
+    }
   });
   return <div>{v}</div>;
 };
